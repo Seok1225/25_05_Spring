10242e52127430e0762a3121b2107819f31eb94e
Seok1225 <koreait6642@gmail.com>
2025-05-19 13:25:06 +0900

    좋아요 기능 구현

--- src/main/java/com/example/demo/service/ReactionPointService.java
+++ src/main/java/com/example/demo/service/ReactionPointService.java
@@ -6,17 +6,21 @@ import org.springframework.stereotype.Service;
 import com.example.demo.repository.BoardRepository;
 import com.example.demo.repository.ReactionPointRepository;
 import com.example.demo.vo.Board;
+import com.example.demo.vo.ResultData;
 
 @Service
 public class ReactionPointService {
 	@Autowired
 	private ReactionPointRepository reactionPointRepository;
 
+	@Autowired
+	private ArticleService articleService;
+
 	public ReactionPointService(ReactionPointRepository reactionPointRepository) {
 		this.reactionPointRepository = reactionPointRepository;
 	}
 
-	public int userCanReaction(int loginedMemberId, String relTypeCode, int relId) {
+	public int usersReaction(int loginedMemberId, String relTypeCode, int relId) {
 
 		// 로그인 안했어
 		if (loginedMemberId == 0) {
@@ -24,6 +28,23 @@ public class ReactionPointService {
 		}
 
 		return reactionPointRepository.getSumReactionPoint(loginedMemberId, relTypeCode, relId);
+	}
+
+	public ResultData increaseReactionPoint(int loginedMemberId, String relTypeCode, int relId) {
+
+		int affectedRow = reactionPointRepository.increaseReactionPoint(loginedMemberId, relTypeCode, relId);
+
+		if (affectedRow != 1) {
+			return ResultData.from("F-2", "좋아요 실패");
+		}
+
+		switch (relTypeCode) {
+		case "article":
+			articleService.increaseGoodReactionPoint(relId);
+			break;
+		}
+
+		return ResultData.from("S-1", "좋아요!");
 	}
 
 }
